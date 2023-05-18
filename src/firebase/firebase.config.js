3d8aa5d6cb2dc6104e2e51dd94b6f5278fb2d298
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-18 16:32:45 +0600

    add firebase and integrated it

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7NOLg0T6fqAK6mUxQeuudcMmPbpQFg6s",
+  authDomain: "toy-town-82e43.firebaseapp.com",
+  projectId: "toy-town-82e43",
+  storageBucket: "toy-town-82e43.appspot.com",
+  messagingSenderId: "746781347665",
+  appId: "1:746781347665:web:5d047108ef7a92706fcd70",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
