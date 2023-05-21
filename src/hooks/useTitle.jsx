914aeba44d66dfeca962d7cab2b88360bf83d9d2
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-21 13:07:22 +0600

    polise sponser

--- src/hooks/useTitle.jsx
+++ src/hooks/useTitle.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const useTitle = (title) => {
   useEffect(() => {
-    document.title = `${title}-Toy Town`;
+    document.title = `${title} | Toy Town`;
   }, [title]);
 };
 
